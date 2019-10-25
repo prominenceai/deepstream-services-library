5d3871985c095ec31721d6df2cc407790d5d8fe7
Michael Patrick <sscsmatrix@gmail.com>
2019-10-25 16:23:35 -0700

    This should add the code that was missing when I tried to commit the first time

--- examples/python/pipeOpenTst.py
+++ examples/python/pipeOpenTst.py
@@ -9,11 +9,34 @@
 #                   the pipeline and then trys to reopen the pipeline
 #
 #######################################################################
+import ctypes 
 
-import ctypes
+def test1() :
+#    libc = ctypes.cdll.LoadLibrary('/home/sscsmatrix/Projects/CanAmMex/deepstream-services-library/dsl-lib.so')
+    libc =ctypes.CDLL("dsl-lib.so")
+    if libc.dsl_pipeline_new("pipeline1") == 0:
+        print("pipeline opened")
+    else:
+        print("pipeline failed to open")
 
-def test1 :
-    libc = ctypes.CDLL("dsl-app.so")
+    # Try to open pipeline again
+    print("Try to open same pipeline it should fail to open")
+    if libc.dsl_pipeline_new("pipeline1") == 0:
+        print("pipeline opened")
+    else:
+        print("pipeline failed to open")
+        print("delete pipeline")
+        if libc.dsl_pipeline_delete("pipeline1") == 0:
+            print("pipeline successfully deleted")
+            print("try to open pipeline again")
+            if libc.dsl_pipeline_new("pipeline1") == 0:
+                 print("pipeline opened")
+                 print("Test passed")
+            else:
+                 print("pipeline failed to open")
+        else:
+            print("test failed!!")
+
 
 if __name__ == "__main__":
     test1()
