3eb32fb2d554e05bd195f71d180c8b80c4728945
rjhowell44 <rjhowell44@gmail.com>
2020-03-18 16:31:53 -0700

    Add new dyn crop example

---
+++ examples/python/1uri_file_pgie_tiler_osd_dyn_crop_window.py
@@ -0,0 +1,208 @@
+################################################################################
+# The MIT License
+#
+# Copyright (c) 2019-2020, Robert Howell. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+################################################################################
+
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, "../../")
+from dsl import *
+import pyds
+
+source_width = 1280
+source_height = 720
+
+PGIE_CLASS_ID_PERSON = 2
+
+uri_file = "../../test/streams/sample_1080p_h264.mp4"
+
+# Filespecs for the Primary GIE
+primary_infer_config_file = '../../test/configs/config_infer_primary_nano.txt'
+primary_model_engine_file = '../../test/models/Primary_Detector_Nano/resnet10.caffemodel_b1_fp16.engine'
+
+def osd_sink_pad_buffer_probe(buffer, user_data):
+    frame_number = 0
+    #Intiallizing object counter with 0.
+    num_rects = 0
+
+    # Retrieve batch metadata from the gst_buffer
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(buffer)
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
+            # The casting is done by pyds.glist_get_nvds_frame_meta()
+            # The casting also keeps ownership of the underlying memory
+            # in the C code, so the Python garbage collector will leave
+            # it alone.
+            frame_meta = pyds.glist_get_nvds_frame_meta(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number = frame_meta.frame_num
+        num_rects = frame_meta.num_obj_meta
+        l_obj = frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                # Casting l_obj.data to pyds.NvDsObjectMeta
+                obj_meta=pyds.glist_get_nvds_object_meta(l_obj.data)
+            except StopIteration:
+                break
+                
+            # On first occurence of a PERSON, crop the stream based on the object's rectangle parameters
+            if obj_meta.class_id == PGIE_CLASS_ID_PERSON:
+
+                retval = dsl_osd_crop_settings_set('on-screen-display',
+                    obj_meta.rect_params.left, obj_meta.rect_params.top, obj_meta.rect_params.width, obj_meta.rect_params.height)
+                if retval != DSL_RETURN_SUCCESS:
+                    print(dsl_return_value_to_string(retval))
+
+                # exit after first occurence of PERSON CLASS    
+                return True
+                
+            try: 
+                l_obj = l_obj.next
+            except StopIteration:
+                break
+        try:
+            l_frame = l_frame.next
+        except StopIteration:
+            break
+    return True
+## 
+# Function to be called on XWindow KeyRelease event
+## 
+def xwindow_key_event_handler(key_string, client_data):
+    print('key released = ', key_string)
+    if key_string.upper() == 'P':
+        dsl_pipeline_pause('pipeline')
+    elif key_string.upper() == 'R':
+        dsl_pipeline_play('pipeline')
+    elif key_string.upper() == 'Q' or key_string == '':
+        dsl_main_loop_quit()
+ 
+## 
+# Function to be called on XWindow Delete event
+## 
+def xwindow_delete_event_handler(client_data):
+    print('delete window event')
+    dsl_main_loop_quit()
+
+# Function to be called on End-of-Stream (EOS) event
+def eos_event_listener(client_data):
+    print('Pipeline EOS event')
+    #dsl_main_loop_quit()
+
+## 
+# Function to be called on every change of Pipeline state
+## 
+def state_change_listener(old_state, new_state, client_data):
+    print('previous state = ', old_state, ', new state = ', new_state)
+    if new_state == DSL_STATE_PLAYING:
+        dsl_pipeline_dump_to_dot('pipeline', "state-playing")
+
+def main(args):
+
+    # Since we're not using args, we can Let DSL initialize GST on first call
+    while True:
+
+        # New URI File Source using the filespec defined above
+        retval = dsl_source_uri_new('uri-source', uri_file, False, 0, 0, 0)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # New Primary GIE using the filespecs above with interval = 0
+        retval = dsl_gie_primary_new('primary-gie', primary_infer_config_file, primary_model_engine_file, 0)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # New KTL Tracker, setting max width and height of input frame
+        retval = dsl_tracker_ktl_new('ktl-tracker', 480, 272)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # New Tiled Display, setting width and height, use default cols/rows set by source count
+        retval = dsl_tiler_new('tiler', source_width, source_height)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+ 
+        # New OSD with clock enabled... .
+        retval = dsl_osd_new('on-screen-display', True)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+            
+        # Add Nvidia's example batch meta handler to the Source Pad of the KTL Tracker
+        retval = dsl_osd_batch_meta_handler_add('on-screen-display', DSL_PAD_SINK, osd_sink_pad_buffer_probe, None)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+        
+        ## New Window Sink, 0 x/y offsets and same dimensions as Tiled Display
+        retval = dsl_sink_window_new('window-sink', 0, 0, source_width, source_height)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # New Pipeline to use with the above components
+        retval = dsl_pipeline_new('pipeline')
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # Add all the components to our pipeline
+        retval = dsl_pipeline_component_add_many('pipeline', 
+            ['uri-source', 'primary-gie', 'tiler', 'on-screen-display', 'window-sink', None])
+#            ['csi-source', 'primary-gie', 'ktl-tracker', 'tiler', 'on-screen-display', 'window-sink', None])
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # Add the XWindow event handler functions defined above
+        retval = dsl_pipeline_xwindow_key_event_handler_add("pipeline", xwindow_key_event_handler, None)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+        retval = dsl_pipeline_xwindow_delete_event_handler_add("pipeline", xwindow_delete_event_handler, None)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # Add the listener callback functions defined above
+        retval = dsl_pipeline_state_change_listener_add('pipeline', state_change_listener, None)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+        retval = dsl_pipeline_eos_listener_add('pipeline', eos_event_listener, None)
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        # Play the pipeline
+        retval = dsl_pipeline_play('pipeline')
+        if retval != DSL_RETURN_SUCCESS:
+            break
+
+        dsl_main_loop_run()
+        retval = DSL_RETURN_SUCCESS
+        break
+
+    # Print out the final result
+    print(dsl_return_value_to_string(retval))
+
+    dsl_pipeline_delete_all()
+    dsl_component_delete_all()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
